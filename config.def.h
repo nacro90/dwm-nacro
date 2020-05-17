ad51f7caf4024604d9570539991e43a63a19e71b
Orcan Tiryakioglu <orcan.tiryakioglu@gmail.com>
2020-05-17 19:32:58 +0300

    Change floating keybinding remove shift

--- config.def.h
+++ config.def.h
@@ -105,7 +105,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_space,  cyclelayout,    {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_space,  cyclelayout,    {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_f,      togglefloating, {0} },
+	{ MODKEY,                       XK_f,      togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
